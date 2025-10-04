74b86012578f19eb82c59e8c021f55194932810b
Arshiya Singh <singharshiya28@gmail.com>
2025-10-05 00:44:53 +0530

    setup app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import BookList from "./pages/BookList";
+import BookDetails from "./pages/BookDetails";
+import AddEditBook from "./pages/AddEditBook";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello Tailwind!
-    </h1>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <div className="p-5">
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/books/:id" element={<BookDetails />} />
+          <Route path="/add-book" element={<AddEditBook />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
